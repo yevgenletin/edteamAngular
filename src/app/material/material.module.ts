0284721ee8d960e1bd1b0e66cef274e00807784f
Zhenia <yevgen.letin@gmail.com>
2019-03-28 18:15:19 +0200

    Inputs

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,25 +3,29 @@ import { CommonModule } from '@angular/common';
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
 import { MatToolbarModule,
           MatButtonModule,
-          MatExpansionModule
+          MatExpansionModule,
+          MatInputModule
          } from '@angular/material';
 import { MatIconModule } from '@angular/material/icon';
 import { MaterialButtonComponent } from './material-button/material-button.component';
+import { MaterialInputComponent } from './material-input/material-input.component';
 
 @NgModule({
-  declarations: [MaterialDemoComponent, MaterialButtonComponent],
+  declarations: [MaterialDemoComponent, MaterialButtonComponent, MaterialInputComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatInputModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatInputModule
   ],
 })
 export class MaterialModule { }
